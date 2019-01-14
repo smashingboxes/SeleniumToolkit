49d0b604085933265b35cdd788084d596f59187a
dwhit88 <darrin.whitley@smashingboxes.com>
2019-01-14 09:38:21 -0500

    Finished code documentation.

--- src/main/java/gateways/GatewayUtils.java
+++ src/main/java/gateways/GatewayUtils.java
@@ -10,18 +10,30 @@ public class GatewayUtils {
 
     public static List<String> stepsOutput;
 
+    /**
+     * Formats the returned {@param jsonString} in a readable format
+     *
+     * @param  jsonString   the JSON string that is returned from the PractiTest API call and will be printed in the
+     *                          console
+     */
     public static String toPrettyFormat(String jsonString) {
         JsonObject json = new JsonParser().parse(jsonString).getAsJsonObject();
 
         return new GsonBuilder().disableHtmlEscaping().setPrettyPrinting().create().toJson(json);
     }
 
-    public static String convertMillis(Long millis){
+    /**
+     * Calculates and returns the test run duration based on the given {@param millis} in a format that is acceptable
+     * to the PractiTest API
+     *
+     * @param  milliSeconds   the absolute time to be convered into two digit hours, minutes, and seconds
+     */
+    public static String convertMillis(Long milliSeconds){
         Long seconds;
         Long minutes;
         Long hours;
 
-        seconds = millis / 1000;
+        seconds = milliSeconds / 1000;
         minutes = seconds / 60;
         seconds = seconds % 60;
         hours = minutes / 60;
@@ -30,6 +42,12 @@ public class GatewayUtils {
         return adjustPartial(hours) + ":" + adjustPartial(minutes) + ":" + adjustPartial(seconds);
     }
 
+    /**
+     * Helper method that makes the hours, minutes, and seconds two digits if they are not two digits already. This
+     * helps with formatting the time that is acceptable to the PractiTest API
+     *
+     * @param  partialTime   the pre-calculated time that needs to be returned as two digits
+     */
     private static String adjustPartial(Long partialTime){
         if (partialTime < 10){
             return "0" + partialTime;
--- src/main/java/gateways/practiTest/PractiTestJSONUtils.java
+++ src/main/java/gateways/practiTest/PractiTestJSONUtils.java
@@ -98,6 +98,17 @@ public class PractiTestJSONUtils {
         if (status == 1){ return "0"; } else { return "1"; }
     }
 
+    /**
+     * Ensures that the message attached to the JSON string for running tests is formatted correctly.
+     *
+     * Note: A message is attached to the string ONLY when the test has failed.
+     * Note: The status code for TestNG is different than the status code for PractiTest
+     * TestNG success return code = 1
+     * PractiTest success return code = 0
+     * PractiTest failed return code = 1
+     *
+     * @param  result	   the TestNG result that contains the test status and error message
+     */
     private static String getMessage(ITestResult result){
         if (result.getStatus() == 1){
             return "";
@@ -106,6 +117,13 @@ public class PractiTestJSONUtils {
         }
     }
 
+    /**
+     * Formats the TestNG error message in a way that is acceptable to the PractiTest API for running tests.
+     *
+     * Note: A message is attached to the string ONLY when the test has failed.
+     *
+     * @param  message	   the error message from the TestNG result according to the test run
+     */
     private static String formatMessage(String message){
         message.replace("\n", " ").replace("\"", "\\\"");
 
@@ -116,10 +134,27 @@ public class PractiTestJSONUtils {
         return message;
     }
 
+    /**
+     * Returns the PASSED/FAILED string for each step according to the step's {@param status}
+     *
+     * Note: A message is attached to the string ONLY when the test has failed.
+     *
+     * @param  status	        the status code returned from the TestNG test run
+     * @param  count	        the iterated step indicating how many steps have been completed out of the total number
+     *                              of steps
+     * @param  outputSize       the total number of captured steps for the test run
+     */
     private static String stepStatus(Integer status, Integer count, Integer outputSize){
         if (count == outputSize - 1 && status != 1){ return "FAILED"; } else { return "PASSED"; }
     }
 
+    /**
+     * Sets up the HttpPost request with the appropriate headers
+     *
+     * @param  uri	        the address that the request is pointing to depending on the type of action being done
+     * @param  json_str	    the JSON string that will be passed through the request
+     * @param  encoding     the base64 encoding that captures the devEmail and apiToken
+     */
     private static HttpPost postRequest(String uri, String json_str, byte[] encoding) throws Exception{
         HttpPost request = new HttpPost(uri);
         request.setEntity(new StringEntity(json_str));
@@ -130,14 +165,29 @@ public class PractiTestJSONUtils {
         return request;
     }
 
+    /**
+     * Calculates the duration of the test run from the given {@param result}
+     *
+     * @param  result	   the TestNG result that contains the start and end time for the test run
+     */
     private static String getDuration(ITestResult result){
         return GatewayUtils.convertMillis(result.getEndMillis() - result.getStartMillis());
     }
 
+    /**
+     * Returns the PractiTest URI for test runs
+     *
+     * @param  projectId    the associated project id identified in PractiTest
+     */
     private static String uriRun(String projectId){
         return "https://api.practitest.com/api/v2/projects/" + projectId + "/runs.json";
     }
 
+    /**
+     * Returns the PractiTest URI for creating an instance
+     *
+     * @param  projectId    the associated project id identified in PractiTest
+     */
     private static String uriInstance(String projectId){
         return "https://api.practitest.com/api/v2/projects/" + projectId + "/instances.json";
     }
