d6145a4bc307ae3976574131d726138921cc26c6
dwhit88 <darrin.whitley@smashingboxes.com>
2019-01-14 09:02:46 -0500

    Still adding documentation but focusing on the PractiTestJSONUtils.java class now

--- src/main/java/gateways/practiTest/PractiTestJSONUtils.java
+++ src/main/java/gateways/practiTest/PractiTestJSONUtils.java
@@ -10,10 +10,24 @@ import org.testng.ITestResult;
 
 public class PractiTestJSONUtils {
 
+    /**
+     * Establishes the base64 encoding that captures the {@param devEmail} and {@param apiToken}
+     *
+     * @param  devEmail	       the developer email associated to the PractiTest account
+     * @param  apiToken 	   the api token associated to the PractiTest account
+     */
     public static byte[] getEncoding(String devEmail, String apiToken){
         return Base64.encodeBase64((devEmail + ":" + apiToken).getBytes());
     }
 
+    /**
+     * Establishes the JSON string and returns the HttpPost request for creating a PractiTest instance
+     *
+     * @param  encoding	       the base64 encoding that captures the devEmail and apiToken
+     * @param  projectId	   the associated project id identified in PractiTest
+     * @param  testSetId	   the associated test set id identified in PractiTest
+     * @param  testId          the associated test id identified in PractiTest
+     */
     public static HttpPost createInstance(byte[] encoding, String projectId, String testSetId, String testId)
             throws Exception {
         String json_str = "{\"data\": { \"type\": \"instances\", \"attributes\": {\"test-id\": " + testId +
@@ -21,6 +35,15 @@ public class PractiTestJSONUtils {
         return postRequest(uriInstance(projectId), json_str, encoding);
     }
 
+    /**
+     * Establishes the JSON string and returns the HttpPost request for running a test case in PractiTest. This also
+     * captures each step and adds it to the JSON string
+     *
+     * @param  encoding	       the base64 encoding that captures the devEmail and apiToken
+     * @param  projectId	   the associated project id identified in PractiTest
+     * @param  testSetId	   the associated test set id identified in PractiTest
+     * @param  testId          the associated test id identified in PractiTest
+     */
     public static HttpPost runTest(byte[] encoding, String projectId, String instanceID, String testSetId,
                                    String testId, ITestResult result) throws Exception{
         String json_str = "{\"data\": { \"type\": \"instances\", \"attributes\": {\"set-id\": " + testSetId + ", " +
@@ -41,6 +64,11 @@ public class PractiTestJSONUtils {
         return postRequest(uriRun(projectId), json_str, encoding);
     }
 
+    /**
+     * Helper method that parses through the {@param responseBody} to return the instance ID
+     *
+     * @param  responseBody	   the response body from the HTTPPost request for creating an instance
+     */
     public static String getInstanceId(String responseBody){
         JsonElement jsonTree = new JsonParser().parse(responseBody);
         String newInstanceId = "";
@@ -56,6 +84,16 @@ public class PractiTestJSONUtils {
         return newInstanceId;
     }
 
+    /**
+     * Helper method that returns the proper test status to be sent to PractiTest.
+     *
+     * Note: The status code for TestNG is different than the status code for PractiTest
+     * TestNG success return code = 1
+     * PractiTest success return code = 0
+     * PractiTest failed return code = 1
+     *
+     * @param  status	   the status code returned from the TestNG test run
+     */
     private static String getTestStatus(Integer status){
         if (status == 1){ return "0"; } else { return "1"; }
     }
